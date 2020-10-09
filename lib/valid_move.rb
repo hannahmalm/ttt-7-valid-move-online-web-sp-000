687e7987eea16dae5015373b4d1086530441761c
Hannah Malm <hannahemalm616@gmail.com>
2020-10-09 22:14:56 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,25 +4,15 @@
 # valid_move = true, then move is valid_move
 # valid_move = false or nil, then move is not a valid_move
 
-# def valid_move?
-#   #if the position is already taken OR the user selection is not between 0 and 8, return nil or false
-#     if position_taken? == true || user_selection != gets.between(0,8)
-#         false
-# #if the position is not already taken AND the user selection is between 0 and 8, return true.
-#     else position_taken? == false && user_selection == gets.between(0,8)
-#         true
-#     end
-# end
-
-# def valid_move?
-#   #if the position is already taken OR the user selection is not between 0 and 8, return nil or false
-#     if position_taken? == true 
-#         false
-# #if the position is not already taken AND the user selection is between 0 and 8, return true.
-#     else position_taken? == false 
-#         true
-#     end
-# end
+def valid_move?(board,index)
+  #if the position is already taken OR the user selection is not between 0 and 8, return nil or false
+    if position_taken? == true || user_selection != gets.between(0,8)
+        false
+#if the position is not already taken AND the user selection is between 0 and 8, return true.
+    else position_taken? == false && user_selection == gets.between(0,8)
+        true
+    end
+end
 
 # #does user selection fall between 0 and 8
 # user_selection.gets.between(0,8)
